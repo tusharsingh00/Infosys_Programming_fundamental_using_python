41a5eacef4ac52e02109e5baea0d5bbb9ce9183e
tusharsingh00 <cse.tusharsingh@gmail.com>
2024-08-19 12:25:53 +0530

    #

--- Python_programming_fundamental_part_2/Pratice_problem_Level_1/pratice_problem_3.py
+++ Python_programming_fundamental_part_2/Pratice_problem_Level_1/pratice_problem_3.py
@@ -1,4 +1,10 @@
 #lex_auth_0127135787454873601143
+# Write a python function to create and return a new dictionary from the given dictionary(item:price).
+
+# Given the following input, create a new dictionary with elements having price more than 200.
+
+# prices = {'ACME': 45.23,'AAPL': 612.78, 'IBM': 205.55,'HPQ': 37.20,'FB': 10.75}
+
 
 def create_new_dictionary(prices):
     new_dict ={}
